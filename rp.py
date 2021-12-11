63cd13538b7e265369d597459867bc8e6f7a203c
Craig Huckson <craig.huckson@gmail.com>
2021-12-11 11:53:10 -0500

    updates

--- rp.py
+++ rp.py
@@ -1,5 +1,8 @@
+# TODO - MAKE SWITH FOR DIFFERENT FORMS BELL PRIM BELL AUX ROGERS PRIM AND AUX
+# TODO - finish update button handling for bell primary and test
+
+
 import datetime
-from inspect import Attribute
 import PySimpleGUI as sg
 import PIL
 from PIL import ImageGrab, ImageFont
@@ -11,33 +14,63 @@ from PySimpleGUI.PySimpleGUI import BUTTON_TYPE_READ_FORM, WIN_CLOSED
 
 d = datetime.datetime.now()
 
-#CONSTANTS
-RPSIZE = (816,1056)
-TOTALPAGES = (776,20)
-UNITS = (765,105)
-MFONT = 'Arial 12 normal'
-LFONT = 'Arial 20 normal'
-NBOUNDARY = (51,243)
-SBOUNDARY = (449,244)
-WBOUNDARY = (55,276)
-EBOUNDARY = (449,274)
-DATE = str('/'.join([d.strftime(x) for x in ['%Y', '%m', '%d']]))
-NAME = 'CRAIG HUCKSON'
-NAMECOORDS = (340,967)
+# CONSTANTS
+PAGESIZE = (816, 1056)
+RP_TOTALPAGES = (776, 20)
+BP_TOTALPAGES = (778, 23)
+RP_UNITS = (765, 105)
+BP_UNITS = (775, 104)
+MFONT = "Arial 12 normal"
+LFONT = "Arial 20 normal"
+NBOUNDARY = (51, 243)
+SBOUNDARY = (449, 244)
+WBOUNDARY = (55, 276)
+EBOUNDARY = (449, 274)
+DATE = str("/".join([d.strftime(x) for x in ["%Y", "%m", "%d"]]))
+NAME = "CRAIG HUCKSON"
+RP_NAMECOORDS = (340, 967)
+BP_NAMECOORDS = (345, 929)
 LOCATORID = 130003
-LOCATORIDCOORDS = (341,1003)
-DATECOORDS = (620,967)
-MOMTEXT = 'x'
-MOMCOORDS = (40,810)
-MEMAILTEXT = 'x'
-MEMAILCOORDS = (544,1002)
-CLEARIMAGECOORDS = (428,536)
-rogclear = {'reg_clear':'rogclear.PNG','ftth':'ftthstamp.PNG','fo_only':'exclusion.PNG'}
+LOCATORIDCOORDS = (341, 1003)
+RP_DATECOORDS = (620, 967)
+BP_DATECOORDS = (368, 985)
+BP_CABLE = (215, 276)
+BP_CONDUIT = (303, 277)
+BP_PAINT = (162, 310)
+BP_FLAGS = (285, 311)
+BP_GUIDELINES = (557, 939)
+BP_MARKEMAIL = (558, 991)
+BP_TEXT = "x"
+RP_MOMTEXT = "x"
+RP_MOMCOORDS = (40, 810)
+RP_MEMAILTEXT = "x"
+MEMAILCOORDS = (544, 1002)
+RP_CLEARIMAGECOORDS = (428, 536)
+rogclear = {
+    "reg_clear": "rogclear.PNG",
+    "ftth": "ftthstamp.PNG",
+    "fo_only": "exclusion.PNG",
+}
 
-#FUNCTIONS
+bell_stickers = {
+    "bridge_f": "bsticker bridge.jpg",
+    "bridge_xy": (401, 497),
+    "priority_f": "bsticker priority.jpg",
+    "priority_xy": (412, 611),
+    "cablesmbinconduit_f": "bsticker cables mb in conduit.jpg",
+    "empty_conduit_f": "bsticker empty conduit.jpg",
+    "empty_conduit_xy": (157, 740),
+    "hand_dig_f": "bsticker hand dig.jpg",
+    "hand_dig_xy": (648, 722),
+    "future_use_f": "bsticker future use.jpg",
+    "future_use_xy": (614, 856),
+}
+
+# FUNCTIONS
+
 
 def convert_to_bytes(file_or_bytes, resize=None):
-    '''
+    """
     Will convert into bytes and optionally resize an image that is a file or a base64 bytes object.
     Turns into  PNG format in the process so that can be displayed by tkinter
     :param file_or_bytes: either a string filename or a bytes base64 image object
@@ -46,7 +79,7 @@ def convert_to_bytes(file_or_bytes, resize=None):
     :type resize: (Tuple[int, int] or None)
     :return: (bytes) a byte-string object
     :rtype: (bytes)
-    '''
+    """
     if isinstance(file_or_bytes, str):
         img = PIL.Image.open(file_or_bytes)
     else:
@@ -59,80 +92,168 @@ def convert_to_bytes(file_or_bytes, resize=None):
     cur_width, cur_height = img.size
     if resize:
         new_width, new_height = resize
-        scale = min(new_height/cur_height, new_width/cur_width)
-        img = img.resize((int(cur_width*scale), int(cur_height*scale)), PIL.Image.ANTIALIAS)
+        scale = min(new_height / cur_height, new_width / cur_width)
+        img = img.resize(
+            (int(cur_width * scale), int(cur_height * scale)), PIL.Image.ANTIALIAS
+        )
     bio = io.BytesIO()
     img.save(bio, format="PNG")
     del img
     return bio.getvalue()
 
-lcol = [
-    [sg.Text('Choose form:'),sg.I(k='infile',enable_events=True),sg.FileBrowse()],
-    [sg.Text('Total pages: '), sg.Input(s=5,key='tp'),sg.Text('Units: '), 
-    sg.Input(size=5,k='lunits')],
-    [sg.Text('North Boundary: '), sg.Input(enable_events=True,key='nb')],
-    [sg.Text('South Boundary: '), sg.Input(enable_events=True,key='sb')],
-    [sg.Text('West Boundary: '), sg.Input(enable_events=True,key='wb')],
-    [sg.Text('East Boundary: '), sg.Input(enable_events=True,key='eb')],
-    [sg.Radio('Marked',group_id='mc',default=True,k='rmarked',enable_events=True), sg.Radio('Clear',group_id='mc', k='rclear',enable_events=True),sg.Text('Clear reason:'),
-     sg.Combo(values=[rogclear['reg_clear'],rogclear['ftth'],rogclear['fo_only']], default_value=rogclear['reg_clear'],enable_events=True, k='clear_reason', visible=False)],
-    [sg.B('Update',BUTTON_TYPE_READ_FORM),sg.Button('Display')]
+
+# LAYOUT
+
+rp_tab = [
+    [
+        sg.Text("Total pages: "),
+        sg.Input(s=5, key="tp"),
+        sg.Text("Units: "),
+        sg.Input(size=5, k="lunits"),
+    ],
+    [sg.Text("North Boundary: "), sg.Input(enable_events=True, key="nb")],
+    [sg.Text("South Boundary: "), sg.Input(enable_events=True, key="sb")],
+    [sg.Text("West Boundary: "), sg.Input(enable_events=True, key="wb")],
+    [sg.Text("East Boundary: "), sg.Input(enable_events=True, key="eb")],
+    [
+        sg.Radio(
+            "Marked", group_id="mc", default=True, k="rmarked", enable_events=True
+        ),
+        sg.Radio("Clear", group_id="mc", k="rclear", enable_events=True),
+        sg.Text("Clear reason:"),
+        sg.Combo(
+            values=[rogclear["reg_clear"], rogclear["ftth"], rogclear["fo_only"]],
+            default_value=rogclear["reg_clear"],
+            enable_events=True,
+            k="clear_reason",
+            visible=False,
+        ),
+    ],
+    [sg.B("Update", BUTTON_TYPE_READ_FORM,k='rpupdate'), sg.Button("Display")],
 ]
 
-rcol = [[sg.Image(size=(640,480),expand_y=True,key='img')]]
+ra_tab = [[sg.Text("todo")]]
 
-layout = [[sg.Column(lcol), sg.Column(rcol,scrollable=True,vertical_scroll_only=True,expand_x=True,expand_y=True)]]
-#tp = input('Total pages: ')
-#lunits = input('Units? ')
-#nbound = input('North boundary?')
-#sbound = input('South boundary?')
-#wbound = input('West boundary? ')
-#ebound = input('East boundary? ')
-#clearmsg = input('Clear message? ')
+bp_tab = [
+    [
+        sg.Text("Total pages: "),
+        sg.Input(s=5, key="bptp"),
+        sg.Text("Units: "),
+        sg.Input(size=5, k="blunits"),
+    ],
+    [sg.Text('Stickers:')],
+    [sg.Checkbox('Bridge',enable_events=True,k='bridge')], 
+    [sg.Checkbox('Priority',enable_events=True,k='priority')],
+    [sg.Checkbox('Cables MB In Conduit',enable_events=True,k='cabcon')],
+    [sg.Checkbox('Hand Dig Only',enable_events=True,k='handdig')],
+    [sg.Checkbox('Empty Conduits',enable_events=True,k='empty')],
+    [sg.Checkbox('Future Use',enable_events=True,k='futureuse')],
+    [sg.B("Update", BUTTON_TYPE_READ_FORM,k='bpupdate'), sg.Button("Display")],
+]
 
-window = sg.Window('Locate form filler', layout, resizable=True,grab_anywhere=True, finalize=True)
+ba_tab = [[sg.Text("todo")]]
+
+lcol = [
+    [
+        sg.Text("Choose form:"), sg.I(k="infile", enable_events=True), sg.FileBrowse()
+    ],
+    [
+        sg.TabGroup(
+            [
+                [
+                    sg.Tab("Bell Primary", bp_tab),
+                    sg.Tab("Bell Auxilliary", ba_tab),
+                    sg.Tab("Rogers Primary", rp_tab),
+                    sg.Tab("Rogers Auxilliary", ra_tab),
+                ]
+            ]
+        )
+    ]
+]
+rcol = [[sg.Image(size=(640, 480), expand_y=True, key="img")]]
+
+layout = [
+    [
+        sg.Column(lcol),
+        sg.Column(
+            rcol,
+            scrollable=True,
+            vertical_scroll_only=True,
+            expand_x=True,
+            expand_y=True,
+        ),
+    ]
+]
+# tp = input('Total pages: ')
+# lunits = input('Units? ')
+# nbound = input('North boundary?')
+# sbound = input('South boundary?')
+# wbound = input('West boundary? ')
+# ebound = input('East boundary? ')
+# clearmsg = input('Clear message? ')
+
+window = sg.Window(
+    "Locate form filler", layout, resizable=True, grab_anywhere=True, finalize=True
+)
 
 while True:
     event, values = window.read()
-    if event in (sg.WIN_CLOSED, 'Exit'):
+    if event in (sg.WIN_CLOSED, "Exit"):
         break
     # shows stuff in right column
-    if event == 'infile':
-        window['img'].update(data=convert_to_bytes(values['infile']))
-    if event == 'rclear':
-        window['clear_reason'].update(visible=True)
-    elif event == 'rmarked':
-        window['clear_reason'].update(visible=False)
-    if event == 'Update':
-        #this writes in the dig area and other shiT
-        with Image.open(values['infile']) as out:
-            fntm = ImageFont.truetype('arialbd.ttf',size=12)
-            fntl = ImageFont.truetype('arialbd.ttf',size=16)
+    if event == "infile":
+        window["img"].update(data=convert_to_bytes(values["infile"]))
+    if event == "rclear":
+        window["clear_reason"].update(visible=True)
+    elif event == "rmarked":
+        window["clear_reason"].update(visible=False)
+    if event == "rpupdate":
+        # this writes in the dig area and other shiT
+        with Image.open(values["infile"]) as out:
+            fntm = ImageFont.truetype("arialbd.ttf", size=12)
+            fntl = ImageFont.truetype("arialbd.ttf", size=16)
             d = ImageDraw.Draw(out)
-            tdict = {
-            TOTALPAGES:str(values['tp']),
-            UNITS:values['lunits'],
-            NBOUNDARY:values['nb'],
-            SBOUNDARY:values['sb'],
-            WBOUNDARY:values['wb'],
-            EBOUNDARY:values['eb'],
-            NAMECOORDS:NAME,
-            DATECOORDS:str(DATE),
-            MEMAILCOORDS:MEMAILTEXT,
-            LOCATORIDCOORDS:str(LOCATORID)
-                    }
+            rp_dict = {
+                RP_TOTALPAGES: str(values["tp"]),
+                RP_UNITS: values["lunits"],
+                NBOUNDARY: values["nb"],
+                SBOUNDARY: values["sb"],
+                WBOUNDARY: values["wb"],
+                EBOUNDARY: values["eb"],
+                RP_NAMECOORDS: NAME,
+                RP_DATECOORDS: str(DATE),
+                MEMAILCOORDS: RP_MEMAILTEXT,
+                LOCATORIDCOORDS: str(LOCATORID),
+            }
 
-            for k,v in tdict.items():
-                d.text(k, v, fill='black', font=fntm)
-            if 'M' in values['lunits']:
-                d.text(MOMCOORDS,MOMTEXT,fill='black',font=fntm)
+            for k, v in rp_dict.items():
+                d.text(k, v, fill="black", font=fntm)
+            if "M" in values["lunits"]:
+                d.text(RP_MOMCOORDS, RP_MOMTEXT, fill="black", font=fntm)
             else:
-                with Image.open(values['clear_reason']) as cr:
-                    out.paste(cr,CLEARIMAGECOORDS)
-            outfile = out.save('file.png')
-        #except AttributeError:
-            #pass
-    if event == 'Display':
-        window['img'].update(data=convert_to_bytes('file.png'))
+                with Image.open(values["clear_reason"]) as cr:
+                    out.paste(cr, RP_CLEARIMAGECOORDS)
+            outfile = out.save("file.png")
+        # except AttributeError:
+        # pass
+
+    if event == 'bpupdate':
+        with Image.open(values["infile"]) as out:
+            fntm = ImageFont.truetype("arialbd.ttf", size=12)
+            fntl = ImageFont.truetype("arialbd.ttf", size=16)
+            d = ImageDraw.Draw(out)
+            bp_dict = {
+            BP_TOTALPAGES:str(values['bptp']),
+            BP_UNITS:values['blunits'],
+            BP_NAMECOORDS: NAME,
+            BP_DATECOORDS: str(DATE),
+            BP_GUIDELINES:BP_TEXT,
+
+        }
+        for k,v in bp_dict.items():
+            d.text(k,v,)
+
+    if event == "Display":
+        window["img"].update(data=convert_to_bytes("file.png"))
 
 window.close()
